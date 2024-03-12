038b63ca693b40a1b56e635b4c565d00dc4b68aa
Rimsha Ubaid <rimshaubaid@Rimshas-MacBook-Air.local>
2024-03-12 19:14:40 +0500

    providers and hooks

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -17,7 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}><Sidebar>{children}</Sidebar></body>
+      <body className={font.className}>
+        <SupabaseProvider>
+          <UserProvider>
+          <Sidebar>{children}</Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
+      </body>
     </html>
   );
 }
